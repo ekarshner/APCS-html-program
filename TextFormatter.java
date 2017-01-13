8972aa4e899309560b5d9d6cc3e89f17193135b6
ekarshner <elijahkarshner@gmail.com>
2017-01-13 00:59:50 -0500

    Add files via upload

--- TextFormatter.java
+++ TextFormatter.java
@@ -1,20 +1,12 @@
 public class TextFormatter{
-	//private String line;
-	private static String line;
+	private String line;
 	
-	public static void main(String [] args){
-		line = "aabaccb";
-		System.out.println(countStrings("a"));
-		System.out.println(countStrings("b"));
-		System.out.println(countStrings("c"));
-	}
-	
-	/*public TextFormatter(String lineToFormat){
+	public TextFormatter(String lineToFormat){
 		line = lineToFormat;
 		return null;
-	}*/
+	}
 	
-	/*private static int findString(String str, int start){
+	private int findString(String str, int start){
 		String character;
 		String rchar;
 		String lchar;
@@ -35,9 +27,9 @@ public class TextFormatter{
 			}
 		}
 		return -1;
-	}*/
+	}
 	
-	/*private static int countStrings(String str){
+	private int countStrings(String str){
 		String character;
 		String rchar;
 		int finalcnt = 0;
@@ -58,27 +50,23 @@ public class TextFormatter{
 			}
 		}
 		return finalcnt;
-	}*/
+	}
 	
-	public static String convertItalics(){
-		int count = 0;
-		String character;
-		String rchar;
-		int finalcnt = 0;
-		for(int i = 0; i < line.length(); i++){
-
-			character = line.substring(i,i+1);
-			if(i+2 > line.length()){
-				rchar = "N/A";}
-			else{
-				rchar = line.substring(i+1, i+2);}
-			
-			if(character.equals(str) && !(rchar.equals(str))){
-				finalcnt += 1;
+	public String convertItalics(){
+		String ans = line;
+		if(findString("_",0) == -1){
+			return line;
+		}
+		if(countStrings("_") % 2 == 0){
+			for(int i = 0; i < line.length(); i++){
+				if(i%2 == 0){
+					ans = ans.replaceFirst("_","<I>");
+				}
+				else{
+					ans = ans.replaceFirst("_","</I>");
+				}
 			}
-		for(int i = 0; i < line.length(); i++){
-			if(character.equals("_")){
-				if(rchar.equals("_")){
-			
+		}
+		return ans;
 	}
 }
